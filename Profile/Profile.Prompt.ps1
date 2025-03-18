8a533eea43b502a8e459f05e3e6dfe1bc2c65960
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-03-17 20:55:19 -0400

    feat: prompt

--- Profile/Profile.Prompt.ps1
+++ Profile/Profile.Prompt.ps1
@@ -1,21 +1,47 @@
-# ---------------------------------------------------------------------
-# PowerShell Profile - Prompt
-# ---------------------------------------------------------------------
+<#
+    .SYNOPSIS
+        PowerShell Profile - Prompt Configuration
+    .DESCRIPTION
+        Configures the PowerShell prompt using Oh-My-Posh.
+    .PARAMETER DisablePrompt
+        Disables custom prompt configuration.
+#>
+Param(
+    [Switch]$DisablePrompt
+)
 
-# Oh-My-Posh Prompt
-# https://ohmyposh.dev/docs/installation
-if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-    $ohMyPoshConfigPath = Join-Path "$Env:POSH_THEMES_PATH" 'wopian.omp.json'
-    if (Test-Path -Path $ohMyPoshConfigPath) {
+Begin {
+    Write-Verbose "[BEGIN]: Profile.Prompt.ps1"
+    if ($DisablePrompt) {
+        Write-Verbose "Prompt customization is disabled. Skipping prompt configuration."
+        return
+    }
+}
+
+Process {
+    Write-Verbose "[PROCESS]: Profile.Prompt.ps1"
+
+    if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
         try {
-            $Expression = (& oh-my-posh init pwsh --config=$ohMyPoshConfigPath --print) -join "`n"
-            $Expression | Invoke-Expression
+            Write-Verbose "Configuring Oh-My-Posh prompt"
+            $ohMyPoshConfigPath = Join-Path "$Env:POSH_THEMES_PATH" 'wopian.omp.json'
+            if (Test-Path -Path $ohMyPoshConfigPath) {
+                $Expression = (& oh-my-posh init pwsh --config=$ohMyPoshConfigPath --print) -join "`n"
+                $Expression | Invoke-Expression
+            } else {
+                Write-Error "Oh-My-Posh configuration path is not valid: $ohMyPoshConfigPath"
+            }
+            Remove-Variable -Name ohMyPoshConfigPath -ErrorAction SilentlyContinue
+            Remove-Variable -Name Expression -ErrorAction SilentlyContinue
         } catch {
             Write-Error "Failed to configure Oh-My-Posh prompt: $_"
         }
     } else {
-        Write-Error "Oh-My-Posh configuration path is not valid: $ohMyPoshConfigPath"
+        Write-Warning "Oh-My-Posh is not installed. Skipping prompt customization."
     }
-    Remove-Variable -Name ohMyPoshConfigPath -ErrorAction SilentlyContinue
-    Remove-Variable -Name Expression -ErrorAction SilentlyContinue
 }
+
+End {
+    Write-Verbose "[END]: Profile.Prompt.ps1"
+}
+
