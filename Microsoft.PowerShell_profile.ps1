24c9bb27c861c877caa904242081680c37d23e18
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-03-17 20:54:26 -0400

    config: alternative profiles

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,8 @@
 # Current User, Current Host Powershell Core v7 $PROFILE:
 # -------------------------------------------------------
 
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -8,23 +8,6 @@ if (-not $env:Path.Contains([System.Environment]::GetEnvironmentVariable('Path',
   $env:Path += [System.Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::User)
 }
 
-# Define the Update-Profile function
-function Update-Profile {
-    param (
-        [string]$ProfilePath
-    )
-    # Implementation of Update-Profile
-    Write-Output "Updating profile at $ProfilePath"
-}
-
-# Define the Edit-Profile function
-function Edit-Profile {
-    param (
-        [string]$ProfilePath
-    )
-    # Implementation of Edit-Profile
-    Write-Output "Editing profile at $ProfilePath"
-}
 
 
 
