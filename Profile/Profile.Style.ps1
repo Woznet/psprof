9fb208ec1c5de054c5a80e96eb834411f0bc9205
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-03-17 20:54:45 -0400

    feat: style

--- Profile/Profile.Style.ps1
+++ Profile/Profile.Style.ps1
@@ -1,100 +1,121 @@
-# ------------------------------------------------------------
-# Profile.Style.ps1 - PowerShell $PSStyle Configuration
-# ------------------------------------------------------------
+<#
+    .SYNOPSIS
+        PowerShell Profile - Style Configuration
+    .DESCRIPTION
+        Configures PowerShell's $PSStyle and console appearance settings.
+    .PARAMETER DisableStyles
+        Disables style configuration.
+#>
+Param(
+    [Switch]$DisableStyles
+)
 
-# --------------------------------------------------------------------
-# VSCode Default Dark Theme
-# --------------------------------------------------------------------
+Begin {
+    Write-Verbose "[BEGIN]: Profile.Style.ps1"
+    if ($DisableStyles) {
+        Write-Verbose "Styles are disabled. Skipping style configuration."
+        return
+    }
+}
 
-if ($PSStyle) {
-    try {
-        #Enable new fancy progress bar for Windows Terminal
-        if ($ENV:WT_SESSION) {
-            $PSStyle.Progress.UseOSCIndicator = $true
-        }
+Process {
+    Write-Verbose "[PROCESS]: Profile.Style.ps1"
 
-        & {
-            $FG = $PSStyle.Foreground
-            $Format = $PSStyle.Formatting
-            $PSStyle.FileInfo.Directory = $FG.Blue
-            $PSStyle.Progress.View = 'Minimal'
-            $PSStyle.Progress.UseOSCIndicator = $true
-            $DefaultColor = $FG.White
-            $Format.Debug = $FG.Magenta
-            $Format.Verbose = $FG.Cyan
-            $Format.Error = $FG.BrightRed
-            $Format.Warning = $FG.Yellow
-            $Format.FormatAccent = $FG.BrightBlack
-            $Format.TableHeader = $FG.BrightBlack
-            $DarkPlusTypeGreen = "`e[38;2;78;201;176m" #4EC9B0 Dark Plus Type color
-            Set-PSReadLineOption -Colors @{
-                Error     = $Format.Error
-                Keyword   = $FG.Magenta
-                Member    = $FG.BrightCyan
-                Parameter = $FG.BrightCyan
-                Type      = $DarkPlusTypeGreen
-                Variable  = $FG.BrightCyan
-                String    = $FG.Yellow
-                Operator  = $DefaultColor
-                Number    = $FG.BrightGreen
-
-                # These colors should be standard
-                # Command            = "$e[93m"
-                # Comment            = "$e[32m"
-                # ContinuationPrompt = "$e[37m"
-                # Default            = "$e[37m"
-                # Emphasis           = "$e[96m"
-                # Number             = "$e[35m"
-                # Operator           = "$e[37m"
-                # Selection          = "$e[37;46m"
+    if ($PSStyle) {
+        try {
+            Write-Verbose "Configuring PSStyle settings"
+            #Enable new fancy progress bar for Windows Terminal
+            if ($ENV:WT_SESSION) {
+                $PSStyle.Progress.UseOSCIndicator = $true
             }
+
+            & {
+                $FG = $PSStyle.Foreground
+                $Format = $PSStyle.Formatting
+                $PSStyle.FileInfo.Directory = $FG.Blue
+                $PSStyle.Progress.View = 'Minimal'
+                $PSStyle.Progress.UseOSCIndicator = $true
+                $DefaultColor = $FG.White
+                $Format.Debug = $FG.Magenta
+                $Format.Verbose = $FG.Cyan
+                $Format.Error = $FG.BrightRed
+                $Format.Warning = $FG.Yellow
+                $Format.FormatAccent = $FG.BrightBlack
+                $Format.TableHeader = $FG.BrightBlack
+                $DarkPlusTypeGreen = "`e[38;2;78;201;176m" #4EC9B0 Dark Plus Type color
+                Set-PSReadLineOption -Colors @{
+                    Error     = $Format.Error
+                    Keyword   = $FG.Magenta
+                    Member    = $FG.BrightCyan
+                    Parameter = $FG.BrightCyan
+                    Type      = $DarkPlusTypeGreen
+                    Variable  = $FG.BrightCyan
+                    String    = $FG.Yellow
+                    Operator  = $DefaultColor
+                    Number    = $FG.BrightGreen
+
+                    # # These colors should be standard
+                    # Command            = "$e[93m"
+                    # Comment            = "$e[32m"
+                    # ContinuationPrompt = "$e[37m"
+                    # Default            = "$e[37m"
+                    # Emphasis           = "$e[96m"
+                    # Number             = "$e[35m"
+                    # Operator           = "$e[37m"
+                    # Selection          = "$e[37;46m"
+                }
+            }
+        } catch {
+            Write-Error "Failed to configure PowerShell styles: $_"
         }
-    } catch {
-        Write-Error "Failed to configure PowerShell styles: $_"
-    }
-} else {
-    Write-Error "\$PSStyle is not available."
-    #Legacy PS5.1 Configuration
-    #ANSI Escape Character
-    $e = [char]0x1b
-    $host.PrivateData.DebugBackgroundColor = 'Black'
-    $host.PrivateData.DebugForegroundColor = 'Magenta'
-    $host.PrivateData.ErrorBackgroundColor = 'Black'
-    $host.PrivateData.ErrorForegroundColor = 'Red'
-    $host.PrivateData.ProgressBackgroundColor = 'DarkCyan'
-    $host.PrivateData.ProgressForegroundColor = 'Yellow'
-    $host.PrivateData.VerboseBackgroundColor = 'Black'
-    $host.PrivateData.VerboseForegroundColor = 'Cyan'
-    $host.PrivateData.WarningBackgroundColor = 'Black'
-    $host.PrivateData.WarningForegroundColor = 'DarkYellow'
+    } else {
+        Write-Verbose "Configuring legacy style settings"
+        try {
+            # ANSI Escape Character
+            $e = [char]0x1b
+            $host.PrivateData.DebugBackgroundColor = 'Black'
+            $host.PrivateData.DebugForegroundColor = 'Magenta'
+            $host.PrivateData.ErrorBackgroundColor = 'Black'
+            $host.PrivateData.ErrorForegroundColor = 'Red'
+            $host.PrivateData.ProgressBackgroundColor = 'DarkCyan'
+            $host.PrivateData.ProgressForegroundColor = 'Yellow'
+            $host.PrivateData.VerboseBackgroundColor = 'Black'
+            $host.PrivateData.VerboseForegroundColor = 'Cyan'
+            $host.PrivateData.WarningBackgroundColor = 'Black'
+            $host.PrivateData.WarningForegroundColor = 'DarkYellow'
 
-    Set-PSReadLineOption -Colors @{
-        Command            = "$e[93m"
-        Comment            = "$e[32m"
-        ContinuationPrompt = "$e[37m"
-        Default            = "$e[37m"
-        Emphasis           = "$e[96m"
-        Error              = "$e[31m"
-        Keyword            = "$e[35m"
-        Member             = "$e[96m"
-        Number             = "$e[35m"
-        Operator           = "$e[37m"
-        Parameter          = "$e[37m"
-        Selection          = "$e[37;46m"
-        String             = "$e[33m"
-        Type               = "$e[34m"
-        Variable           = "$e[96m"
-    }
+            Set-PSReadLineOption -Colors @{
+                Command            = "$e[93m"
+                Comment            = "$e[32m"
+                ContinuationPrompt = "$e[37m"
+                Default            = "$e[37m"
+                Emphasis           = "$e[96m"
+                Error              = "$e[31m"
+                Keyword            = "$e[35m"
+                Member             = "$e[96m"
+                Number             = "$e[35m"
+                Operator           = "$e[37m"
+                Parameter          = "$e[37m"
+                Selection          = "$e[37;46m"
+                String             = "$e[33m"
+                Type               = "$e[34m"
+                Variable           = "$e[96m"
+            }
 
-    Remove-Variable e
+            Remove-Variable e
+        } catch {
+            Write-Error "Failed to configure legacy styles: $_"
+        }
+    }
+}
+
+End {
+    Write-Verbose "[END]: Profile.Style.ps1"
 }
 
 [Console]::Title = if ($ENV:WT_SESSION) {
-    #Short title for Windows Terminal since we have an icon that lets us already know its PowerShell
+    # Short title for Windows Terminal since we have an icon that lets us already know its PowerShell
     "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)"
-} elseif ($ENV:AZUREPS_HOST_ENVIRONMENT -like 'cloud-shell*') {
-    #Best way I found to get the tenant name of where cloud shell is running
-    "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor) $((Get-AzTenant -TenantId (Get-AzSubscription -SubscriptionId (Get-SubscriptionIdFromStorageProfile)).HomeTenantId).Name)"
 } else {
     "PowerShell $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)"
 }
