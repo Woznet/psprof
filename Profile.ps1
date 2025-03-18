852d09f7e7140e1c184eed11fa8eda7cf487101d
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-03-17 20:54:01 -0400

    feat: add zoxide

--- Profile.ps1
+++ Profile.ps1
@@ -77,3 +77,8 @@ if (-not $NoImports) {
     }
   }
 }
+
+# setup zoxide
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+}
